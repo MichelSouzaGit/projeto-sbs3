1fd18aaf8ee21ea67dea394add33ac464b1e92d3
Michel Souza <michels.git@gmail.com>
2021-09-07 16:02:21 -0300

    Basic static components v2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,8 +8,8 @@ function App() {
       <NavBar />
       <div className="container">
         <h1 className="text-primary">Olá mundo..</h1>
+        <DataTable />
       </div>
-      <DataTable />
       <Footer />
     </>
   );
